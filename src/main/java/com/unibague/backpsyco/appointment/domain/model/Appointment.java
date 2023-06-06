dc2570d17985791351850ffa77fe5c838752f112
Jhon Bravo <jhonedisonbravocasas@hotmail.com>
2023-06-05 23:26:39 -0500

    Correction for DTO Appointment

--- src/main/java/com/unibague/backpsyco/appointment/domain/model/Appointment.java
+++ src/main/java/com/unibague/backpsyco/appointment/domain/model/Appointment.java
@@ -1,5 +1,9 @@
 package com.unibague.backpsyco.appointment.domain.model;
 
+import com.unibague.backpsyco.patient.domain.model.Patient;
+import com.unibague.backpsyco.psychologist.domain.model.Psychologist;
+import com.unibague.backpsyco.service.domain.model.Service;
+import com.unibague.backpsyco.state.domain.model.State;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +16,8 @@ import java.sql.Timestamp;
 public class Appointment {
     private int id;
     private Timestamp date;
-    private int serviceId;
-    private int psychologistId;
-    private int patientId;
-    private int stateId;
+    private Service service;
+    private Psychologist psychologist;
+    private Patient patient;
+    private State state;
 }
--- src/main/java/com/unibague/backpsyco/appointment/infraestructure/mapper/AppointmentMapper.java
+++ src/main/java/com/unibague/backpsyco/appointment/infraestructure/mapper/AppointmentMapper.java
@@ -2,18 +2,51 @@ package com.unibague.backpsyco.appointment.infraestructure.mapper;
 
 import com.unibague.backpsyco.appointment.domain.model.Appointment;
 import com.unibague.backpsyco.appointment.infraestructure.driveradapter.AppointmentData;
+import com.unibague.backpsyco.service.domain.model.Service;
+import com.unibague.backpsyco.psychologist.domain.model.Psychologist;
+import com.unibague.backpsyco.patient.domain.model.Patient;
+import com.unibague.backpsyco.state.domain.model.State;
 
 import java.sql.Timestamp;
 
 public class AppointmentMapper {
-    public static Appointment fromData(AppointmentData appointmentData) {
-        return new Appointment(
-                appointmentData.getId(),
-                new Timestamp(appointmentData.getDate().getTime()),
-                appointmentData.getService().getId(),
-                appointmentData.getPsychologist().getId(),
-                appointmentData.getPatient().getId(),
-                appointmentData.getState().getId()
-        );
+    public static Appointment fromData(AppointmentData data) {
+        Appointment appointment = new Appointment();
+        appointment.setId(data.getId());
+        appointment.setDate(new Timestamp(data.getDate().getTime()));
+
+        Service service = new Service();
+        service.setId(data.getService().getId());
+        service.setServiceName(data.getService().getServiceName());
+        service.setDescription(data.getService().getDescription());
+        service.setCost(data.getService().getCost());
+
+        Psychologist psychologist = new Psychologist();
+        psychologist.setId(data.getPsychologist().getId());
+        psychologist.setName(data.getPsychologist().getName());
+        psychologist.setLastName(data.getPsychologist().getLastName());
+        psychologist.setEmail(data.getPsychologist().getEmail());
+        psychologist.setSpecialtyId(data.getPsychologist().getSpecialty().getId());
+        psychologist.setRoleId(data.getPsychologist().getRole().getId());
+
+        Patient patient = new Patient();
+        patient.setId(data.getPatient().getId());
+        patient.setName(data.getPatient().getName());
+        patient.setLastName(data.getPatient().getLastName());
+        patient.setEmail(data.getPatient().getEmail());
+        patient.setAge(data.getPatient().getAge());
+        patient.setPhone(data.getPatient().getPhone());
+        patient.setPatientTypeId(data.getPatient().getPatientType().getId());
+        patient.setGenderId(data.getPatient().getGender().getId());
+
+        State state = new State();
+        state.setId(data.getState().getId());
+        state.setState(data.getState().getState());
+
+        appointment.setService(service);
+        appointment.setPsychologist(psychologist);
+        appointment.setPatient(patient);
+        appointment.setState(state);
+        return appointment;
     }
 }
